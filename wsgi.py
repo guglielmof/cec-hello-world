5c871d18b978e7f5c68b022cbd16dc596fa24f63
guglielmof <ggfaggioli@gmail.com>
2018-02-17 17:23:01 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     
-    return "Hello World! Gretings from "+socket.gethostname()+"\n"
+    return "Hello World! Greretings from "+socket.gethostname()+"\n"
 
 
 if __name__ == "__main__":
