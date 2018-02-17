60c38382eaba59066923039ca073e3b752b2303e
guglielmof <ggfaggioli@gmail.com>
2018-02-17 20:31:30 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,13 @@
 import socket
+import time
 from flask import Flask
 
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    fh = open("/mnt/hello.txt", "w")
-    lines_of_text = ["a line of text", "another line of text", "a third line"]
+    fh = open("/mnt/hello.txt", "a")
+    lines_of_text = ""+socket.gethostname()+": "+time.time()+"\n"
     fh.writelines(lines_of_text)
     fh.close()
     
