56f29f8d8ccf8868add80b8ae6c7e952ad454902
guglielmof <ggfaggioli@gmail.com>
2018-02-17 17:46:40 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,10 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
+    fh = open("/mnt/hello.txt", "w")
+    lines_of_text = ["a line of text", "another line of text", "a third line"]
+    fh.writelines(lines_of_text)
+    fh.close()
     
     return "Hello World! Greetings from "+socket.gethostname()+"\n"
 
