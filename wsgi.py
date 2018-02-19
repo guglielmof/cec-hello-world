2ab1b836ef785d1bdeb13bf4c6e05fef2f44d2ae
guglielmof <ggfaggioli@gmail.com>
2018-02-19 18:40:10 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,22 +1,26 @@
 import socket
 import time
+import datetime
 from flask import Flask
 
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    fh = open("/mnt/hello.txt", "a")
-    lines_of_text = ""+socket.gethostname()+": "+time.time()+"\n"
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    fh = open("/mnt/logfile.txt", "a")
+    lines_of_text = ""+socket.gethostname()+": "+st+"\n"
     fh.writelines(lines_of_text)
     fh.close()
-    
+
     #return "Hello World! Greetings from "+socket.gethostname()+"\n"
 
-    fh = open("/mnt/hello.txt", "r")
+    fh = open("/mnt/logfile.txt", "r")
     s =""
     for i in fh:
         s += i
+    fh.close()
     return s
 
 if __name__ == "__main__":
